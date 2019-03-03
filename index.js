263141937b4c0193575aa4123b5d00f4b6f5d486
Maykon Deykon <maykondeykon@gmail.com>
2019-03-03 19:23:37 -0300

    Adding Drizzle to project

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import React from 'react';
+import {Drizzle, generateStore} from 'drizzle';
+import MyStringStore from './build/contracts/MyStringStore.json';
+
+const options = {
+    contracts:[MyStringStore]
+};
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+AppRegistry.registerComponent(appName, () => () => <App drizzle={drizzle}/>);
